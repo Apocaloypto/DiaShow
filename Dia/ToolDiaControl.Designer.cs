6311fa8f3cce8251419aec48a4b2fcbc4982f6d1
Apocaloypto <51018969+Apocaloypto@users.noreply.github.com>
2022-04-15 01:19:20 +0200

    ASDF

--- Dia/ToolDiaControl.Designer.cs
+++ Dia/ToolDiaControl.Designer.cs
@@ -33,7 +33,6 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.progTimer = new System.Windows.Forms.ProgressBar();
          this.btnReverse = new System.Windows.Forms.Button();
          this.btnPlayPause = new Dia.PlayPauseButton();
          this.btnForward = new System.Windows.Forms.Button();
@@ -41,23 +40,13 @@
          this.tableLayoutPanel1.SuspendLayout();
          this.SuspendLayout();
          // 
-         // progTimer
-         // 
-         this.tableLayoutPanel1.SetColumnSpan(this.progTimer, 3);
-         this.progTimer.Dock = System.Windows.Forms.DockStyle.Fill;
-         this.progTimer.Location = new System.Drawing.Point(3, 3);
-         this.progTimer.Name = "progTimer";
-         this.progTimer.Size = new System.Drawing.Size(251, 24);
-         this.progTimer.Step = 1;
-         this.progTimer.TabIndex = 0;
-         // 
          // btnReverse
          // 
          this.btnReverse.Dock = System.Windows.Forms.DockStyle.Fill;
          this.btnReverse.Image = global::Dia.Properties.Resources.back;
-         this.btnReverse.Location = new System.Drawing.Point(3, 33);
+         this.btnReverse.Location = new System.Drawing.Point(3, 3);
          this.btnReverse.Name = "btnReverse";
-         this.btnReverse.Size = new System.Drawing.Size(79, 24);
+         this.btnReverse.Size = new System.Drawing.Size(79, 30);
          this.btnReverse.TabIndex = 1;
          this.btnReverse.UseVisualStyleBackColor = true;
          this.btnReverse.Click += new System.EventHandler(this.OnBtnClickedPrev);
@@ -66,11 +55,11 @@
          // 
          this.btnPlayPause.Dock = System.Windows.Forms.DockStyle.Fill;
          this.btnPlayPause.IsPlaying = false;
-         this.btnPlayPause.Location = new System.Drawing.Point(88, 33);
+         this.btnPlayPause.Location = new System.Drawing.Point(88, 3);
          this.btnPlayPause.Name = "btnPlayPause";
          this.btnPlayPause.PauseImage = global::Dia.Properties.Resources.pause;
          this.btnPlayPause.PlayImage = global::Dia.Properties.Resources.play;
-         this.btnPlayPause.Size = new System.Drawing.Size(79, 24);
+         this.btnPlayPause.Size = new System.Drawing.Size(79, 30);
          this.btnPlayPause.TabIndex = 2;
          this.btnPlayPause.UseVisualStyleBackColor = true;
          this.btnPlayPause.Click += new System.EventHandler(this.OnClickedPlayPause);
@@ -79,9 +68,9 @@
          // 
          this.btnForward.Dock = System.Windows.Forms.DockStyle.Fill;
          this.btnForward.Image = global::Dia.Properties.Resources.forward;
-         this.btnForward.Location = new System.Drawing.Point(173, 33);
+         this.btnForward.Location = new System.Drawing.Point(173, 3);
          this.btnForward.Name = "btnForward";
-         this.btnForward.Size = new System.Drawing.Size(81, 24);
+         this.btnForward.Size = new System.Drawing.Size(81, 30);
          this.btnForward.TabIndex = 3;
          this.btnForward.UseVisualStyleBackColor = true;
          this.btnForward.Click += new System.EventHandler(this.OnBtnClickedNext);
@@ -95,23 +84,22 @@
          this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
          this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
          this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-         this.tableLayoutPanel1.Controls.Add(this.progTimer, 0, 0);
-         this.tableLayoutPanel1.Controls.Add(this.btnForward, 2, 1);
-         this.tableLayoutPanel1.Controls.Add(this.btnReverse, 0, 1);
-         this.tableLayoutPanel1.Controls.Add(this.btnPlayPause, 1, 1);
+         this.tableLayoutPanel1.Controls.Add(this.btnForward, 2, 0);
+         this.tableLayoutPanel1.Controls.Add(this.btnReverse, 0, 0);
+         this.tableLayoutPanel1.Controls.Add(this.btnPlayPause, 1, 0);
          this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
          this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-         this.tableLayoutPanel1.RowCount = 2;
-         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-         this.tableLayoutPanel1.Size = new System.Drawing.Size(257, 60);
+         this.tableLayoutPanel1.RowCount = 1;
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+         this.tableLayoutPanel1.Size = new System.Drawing.Size(257, 36);
          this.tableLayoutPanel1.TabIndex = 4;
          // 
          // ToolDiaControl
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(281, 84);
+         this.ClientSize = new System.Drawing.Size(281, 60);
          this.Controls.Add(this.tableLayoutPanel1);
          this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
          this.Name = "ToolDiaControl";
@@ -122,8 +110,6 @@
       }
 
       #endregion
-
-      private ProgressBar progTimer;
       private Button btnReverse;
       private PlayPauseButton btnPlayPause;
       private Button btnForward;
