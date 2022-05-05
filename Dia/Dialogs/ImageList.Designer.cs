9a1bba73a53bf7a0115fa4e693374f2eb86c17c5
Apocaloypto <51018969+Apocaloypto@users.noreply.github.com>
2022-05-05 21:23:46 +0200

    Aufräumen nicht vergessen

--- Dia/Dialogs/ImageList.Designer.cs
+++ Dia/Dialogs/ImageList.Designer.cs
@@ -13,9 +13,15 @@
       /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
       protected override void Dispose(bool disposing)
       {
-         if (disposing && (components != null))
+         if (disposing)
          {
-            components.Dispose();
+            if (components != null)
+            {
+               components.Dispose();
+            }
+
+            _diaController.LoadFile -= _diaController_LoadFile;
+            _diaController.MatchingFilesReloaded -= _diaController_MatchingFilesReloaded;
          }
          base.Dispose(disposing);
       }
